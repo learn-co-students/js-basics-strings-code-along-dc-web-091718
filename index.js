c3c795d4c9e75157f8485767e412afaa9be323ff
DeCode2018 <decode2018@gmail.com>
2018-10-27 17:05:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+let currentUser = 'DUSTIN';
+let welcomeMessage = 'Welcome to Flatbook, '+ currentUser + '!';
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + '!';
+
+let shortGreeting = "Welcome, D!";
